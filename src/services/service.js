ce82a088b59354a4f980d73d7224acc8e487504e
priyanshu-rathore <priyanshurathore132@gmail.com>
2023-05-15 09:47:53 +0530

    final commit

--- src/services/service.js
+++ src/services/service.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
+let headerConfig  = {
+  headers:{
+      "Access-Control-Allow-Origin": "true"
+  }
+}
+
 export const Signin = async (obj)=>{
-let response =  await axios.post('http://fundoonotes.incubation.bridgelabz.com/api/user/login',obj)
+let response =  await axios.post('http://fundoonotes.incubation.bridgelabz.com/api/user/login',obj,headerConfig)
 return response
 }
 
 export const userSignUp = async (obj) =>{
-  let response  = await axios.post('http://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp',obj)
+  let response  = await axios.post('http://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp',obj,headerConfig)
   return response
 }
